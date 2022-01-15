105aa2bfd68879929807a2f820b35c718e850370
Frederic Woelffel <hello@fwoelffel.me>
2022-01-15 16:27:55 +0100

    feat: remove gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,21 @@
-const PUBLIC_URL = process.env.PUBLIC_URL || 'http://localhost:8000';
-
-const fullName = 'Frédéric Woelffel';
-const shortName = 'F. Woelffel';
-const position = 'Back-end engineer';
-
 module.exports = {
   siteMetadata: {
-    title: fullName,
-    position,
-    siteUrl: PUBLIC_URL,
+    title: 'Frédéric Woelffel',
+    position: 'Back-end engineer',
+    siteUrl: process.env.PUBLIC_URL || 'http://localhost:8000',
     about: `I'm a Node.js expert and javascript/typescript developer, fluent with Nestjs.`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: fullName,
-        short_name: shortName,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
   ],
 };
