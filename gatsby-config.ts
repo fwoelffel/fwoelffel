72d4defac91ac38fa281245801284a810ee6f775
Frederic Woelffel <hello@fwoelffel.me>
2023-02-10 00:39:19 +0100

    wip

---
+++ gatsby-config.ts
@@ -0,0 +1,44 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Frédéric Woelffel',
+    position: 'Back-end engineer',
+    siteUrl: process.env.PUBLIC_URL || 'http://localhost:8000',
+    about: `I'm a Node.js expert and javascript/typescript developer, fluent with Nestjs.`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Add your gatsby-remark-* plugins here
+        plugins: [],
+        // Enable JS for https://github.com/jonschlinkert/gray-matter#optionsengines (default: false)
+        // It's not advised to set this to "true" and this option will likely be removed in the future
+        jsFrontmatterEngine: false,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `posts`,
+        // Path to the directory
+        path: `${__dirname}/posts/`,
+      },
+    },
+  ],
+};
