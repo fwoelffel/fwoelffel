80cb26dacd324922c292f1be2a26be5966dee077
Frederic Woelffel <hello@fwoelffel.me>
2023-02-13 15:41:04 +0100

    feat: add title, description, and time to read to posts

--- posts/lorem.md
+++ posts/lorem.md
@@ -13,3 +13,8 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum urna in 
 Aliquam maximus elit interdum mi volutpat tempor. Nam vel mauris quis enim dictum efficitur nec in erat. Suspendisse potenti. Pellentesque vel urna et justo venenatis tincidunt non ac mi. Praesent lobortis massa non risus tincidunt tempus. Etiam sed sem scelerisque ligula porta dignissim ac id magna. Ut ac ipsum et ligula elementum finibus. Vivamus quis lorem consectetur, accumsan nulla ut, ultrices magna. Nullam laoreet purus suscipit eleifend consequat. Aenean sit amet interdum lectus. Sed pretium dolor nec magna blandit ullamcorper. Phasellus ultrices ornare pellentesque. Cras elementum libero tortor, sed auctor nulla iaculis accumsan.
 
 Nunc pulvinar id velit lobortis sagittis. Integer tortor odio, ullamcorper et quam vitae, aliquam fermentum ante. Vestibulum laoreet, magna vitae ultricies ullamcorper, libero eros ultrices enim, ut tempor ipsum diam id metus. Duis tempus ultrices nisi fermentum porta. Nullam vitae sapien nec mi facilisis mattis. Nam vitae nibh pulvinar, semper nunc ut, maximus purus. Ut pharetra hendrerit odio ac pretium. Aliquam vestibulum est eget sapien gravida efficitur. Integer luctus elit et purus mollis, ut laoreet elit fringilla. Donec sed sem ac purus elementum mollis a sit amet turpis. Ut nunc ex, aliquam et ipsum vel, accumsan tincidunt nulla. Maecenas maximus enim ac risus pretium, nec maximus enim blandit.
+
+```js
+console.log('hello world');
+const foo = 'bar';
+```
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,5 +1,6 @@
 import { graphql } from 'gatsby';
 import * as React from 'react';
+import { FaBookOpen } from 'react-icons/fa';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
 
@@ -8,6 +9,7 @@ export const query = graphql`
     markdownRemark(id: { eq: $id }) {
       html
       id
+      timeToRead
       frontmatter {
         date
         title
@@ -16,23 +18,46 @@ export const query = graphql`
   }
 `;
 
-export default function PostPage({ data }) {
-  const { markdownRemark: post } = data;
+export const Head = ({ data: { markdownRemark: post } }) => (
+  <>
+    <title>{post.frontmatter.title}</title>
+    <meta name='description' content={post.excerpt} />
+  </>
+);
+
+export default function PostPage({ data: { markdownRemark: post } }) {
   return (
     <Layout>
       <Hero>
         <article>
-          <div>
-            <h2 className={'title is-2'}>{post.frontmatter.title}</h2>
-            <p className={'subtitle is-spaced'}>{post.frontmatter.date}</p>
-            <section className={'content'}>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: post.html,
-                }}
-              />
-            </section>
+          <div className={'level'}>
+            <div className={'level-left'}>
+              <div className={'level-item'}>
+                <div>
+                  <p className={'title is-2'}>{post.frontmatter.title}</p>
+                  <p className={'has-text-weight-light'}>
+                    {post.frontmatter.date}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={'level-right'}>
+              <div className={'level-item has-text-grey-light'}>
+                <FaBookOpen className={'icon is-small mr-1'} />
+                <p className={'has-text-weight-light'}>
+                  {post.timeToRead} minute{post.timeToRead > 1 ? 's' : ''}
+                </p>
+              </div>
+            </div>
           </div>
+
+          <section className={'content has-text-justified	'}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.html,
+              }}
+            />
+          </section>
         </article>
       </Hero>
     </Layout>
