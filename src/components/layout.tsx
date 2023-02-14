25fae2bb83adbe5899169050ba674bd77cfba913
Frederic Woelffel <hello@fwoelffel.me>
2023-02-14 15:56:50 +0100

    feat: add scroll to top button to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,12 @@
 import Footer from './footer';
 import React from 'react';
+import ScrollButton from './scroll-button';
 
 const Layout = ({ children }) => (
   <>
     <div className='container'>{children}</div>
     <Footer />
+    <ScrollButton />
   </>
 );
 export default Layout;
---
+++ src/components/scroll-button.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { FaAngleUp } from 'react-icons/fa';
+
+export default function ScrollButton() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+  return (
+    <div
+      className={'container has-text-right pr-5'}
+      style={{
+        display: visible ? 'inline' : 'none',
+        position: 'fixed',
+        width: '100%',
+        bottom: '40px',
+        height: '20px',
+        cursor: 'pointer',
+        opacity: '0.9',
+      }}>
+      <button className={'button is-outlined is-rounded'}>
+        {' '}
+        <FaAngleUp onClick={scrollToTop} size='1.5rem' />
+      </button>
+    </div>
+  );
+}
