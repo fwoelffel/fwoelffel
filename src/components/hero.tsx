421affb725cbc972db37328a54b583e1058423d3
Frederic Woelffel <frederic@merca.team>
2025-01-28 18:46:03 +0100

    fix hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Hero = ({ children }) => (
   <section className={'hero is-fullheight'}>
-    <div className={'hero-body container'}>{children}</div>
+    <div className={'hero-body'}>
+      <div className={'container has-text-centered'}>
+        {children}
+      </div>
+    </div>
   </section>
 );
 
